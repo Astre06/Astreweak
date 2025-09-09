3ea2fd7e7f2d364000a26aa541790a75523ba39b
Astre06 <122554381+Astre06@users.noreply.github.com>
2025-09-09 17:18:25 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 # config.py
 
 # Telegram Bot Configuration
-TELEGRAM_BOT_TOKEN = "8340045274:AAGX34z7G_FNNNwDbXuGMQtftp4Y7MaoPzQ"
+TELEGRAM_BOT_TOKEN = "8326116065:AAHp7RYKFD4Kb-li_uebIsmzgFJoBVA9SHU"
 TELEGRAM_CHAT_ID = "6679042143"
 
 # Stripe and API Configuration
-DEFAULT_API_URL = "https://www.nutritionaledge.co.uk/my-account/add-payment-method/"
+DEFAULT_API_URL = "https://jensgetfitgroup.com/my-account/add-payment-method/"
 STRIPE_URL = "https://api.stripe.com/v1/payment_methods"
 
 # Processing Configuration
 MAX_WORKERS = 5
 RETRY_COUNT = 3
+
 RETRY_DELAY = 1
